37f788b887bc9aaac1167d8368ea87c7a1571817
dianaz <stingray2091@gmail.com>
2020-06-13 15:38:42 -0400

    linked index.html to index.js

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 const root = { root: path.join(__dirname, "/public") };
-const { htmlRoutes } = require('./routes/htmlRoutes.js');
+
 //const apiRoutes = require('./routes/apiRoutes');
-console.log(htmlRoutes);
 // set up our middleware to handle POST data
-//app.use(express.urlencoded({ extended: true }));
-//app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(root.root));
 
 //app.use('/api', apiRoutes);
 //app.use(htmlRoutes);
 //htmlRoutes(app);
 app.get('/', (req, res) => res.sendFile("index.html", root));
-app.get('/notes', (req, res) => res.sendFile("notes.html", root));
+app.get('/notes', (req, res) => res.sendFile("./notes.html", root));
 
 
 
